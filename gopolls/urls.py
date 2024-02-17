83ca89f6dcb02afd2cf9fc59d7fca2ffb546c9c4
BMVG-dev <zezeballoon@gmail.com>
2024-02-17 03:05:29 +0000

    Conecte essas novas visualizações ao módulo gopolls.urls adicionando as seguintes chamadas path(): em gopolls/urls.py

--- gopolls/urls.py
+++ gopolls/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('<int:question_id>/', views.detail_page, name='detail_page'),
+    path('<int:question_id>/results_page', views.results_page, name='results_page'),
+    path('<int:question_id>/vote_page', views.vote_page, name='vote_page'),
 
 ]
--- gopolls/views.py
+++ gopolls/views.py
@@ -7,14 +7,14 @@ def home_page(request):
     return HttpResponse(" Hello, World...you're at the Home_page ")
 
 
-def detail(request, question_id):
+def detail_page(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
 
-def results(request, question_id):
+def results_page(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
 
-def vote(request, question_id):
+def vote_page(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
